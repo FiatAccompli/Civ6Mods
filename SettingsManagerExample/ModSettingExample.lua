dd3ef97f4c050a3c55ffdd847666de3d175fc82d
FiatAccompli <kalittle@u.washington.edu>
2018-11-24 02:55:41 -0800

    Updated for settingsmanager api changes.

--- SettingsManagerExample/ModSettingExample.lua
+++ SettingsManagerExample/ModSettingExample.lua
@@ -1,7 +1,11 @@
 -- A very simple example of using mod setting provided by the Mod Settings Manager mod.
 -- All this does is print out the values of the declared settings when they are changed.
 
-include ("ModSettings");
+include("ModSettings");
+include("mod_settings_key_binding_helper");
+
+local actionSetting = ModSettings.Action:new(
+  "LOC_MOD_SETTING_EXAMPLE_CATEGORY", "LOC_MOD_SETTING_EXAMPLE_KEY_ACTION_SETTING", "LOC_MOD_SETTING_EXAMPLE_SETTING_TOOLTIP");
 
 local booleanSetting = ModSettings.Boolean:new(false, "LOC_MOD_SETTING_EXAMPLE_CATEGORY", 
   "LOC_MOD_SETTING_EXAMPLE_BOOLEAN_SETTING", "LOC_MOD_SETTING_EXAMPLE_SETTING_TOOLTIP");
@@ -9,11 +13,11 @@ local booleanSetting = ModSettings.Boolean:new(false, "LOC_MOD_SETTING_EXAMPLE_C
 local booleanSetting2 = ModSettings.Boolean:new(false, "LOC_MOD_SETTING_EXAMPLE_CATEGORY", 
   "LOC_MOD_SETTING_EXAMPLE_BOOLEAN_SETTING_2", "LOC_MOD_SETTING_EXAMPLE_SETTING_TOOLTIP");
 
-local steppedRangeSetting = ModSettings.Range:new(50, 0, 100, 10, "LOC_MOD_SETTING_EXAMPLE_CATEGORY", 
+local steppedRangeSetting = ModSettings.Range:new(50, 0, 100, "LOC_MOD_SETTING_EXAMPLE_CATEGORY", 
   "LOC_MOD_SETTING_EXAMPLE_STEPPED_RANGE_SETTING", "LOC_MOD_SETTING_EXAMPLE_SETTING_TOOLTIP", 
-  ModSettings.Range.PERCENT_FORMATTER);
+  { Steps = 10, ValueFormatter = ModSettings.Range.PERCENT_FORMATTER });
 
-local rangeSetting = ModSettings.Range:new(0, 0, 100, nil, "LOC_MOD_SETTING_EXAMPLE_CATEGORY", 
+local rangeSetting = ModSettings.Range:new(0, 0, 100, "LOC_MOD_SETTING_EXAMPLE_CATEGORY", 
   "LOC_MOD_SETTING_EXAMPLE_RANGE_SETTING", "LOC_MOD_SETTING_EXAMPLE_SETTING_TOOLTIP");
 
 local textSetting = ModSettings.Text:new("Default value", "LOC_MOD_SETTING_EXAMPLE_CATEGORY",
@@ -25,15 +29,22 @@ local selectValues = {"LOC_MOD_SETTING_EXAMPLE_SELECT_SETTING_VALUE_1",
 local selectSetting = ModSettings.Select:new(selectValues, 2, "LOC_MOD_SETTING_EXAMPLE_CATEGORY",
   "LOC_MOD_SETTING_EXAMPLE_SELECT_SETTING", "LOC_MOD_SETTING_EXAMPLE_SETTING_TOOLTIP");
 
-local keybindSetting = ModSettings.KeyBinding:new(ModSettings.KeyBinding.MakeValue(Keys.T, true, false, false), 
+-- Note that this will show the "conflict" UI for key bindings since they both have the same default.
+local keybindSetting = ModSettings.KeyBinding:new(ModSettings.KeyBinding.MakeValue(Keys.B, {Alt=true, Ctrl=true}),
   "LOC_MOD_SETTING_EXAMPLE_CATEGORY", "LOC_MOD_SETTING_EXAMPLE_KEY_BINDING_SETTING", "LOC_MOD_SETTING_EXAMPLE_SETTING_TOOLTIP");
+local keybindSetting2 = ModSettings.KeyBinding:new(ModSettings.KeyBinding.MakeValue(Keys.B),
+  "LOC_MOD_SETTING_EXAMPLE_CATEGORY", "LOC_MOD_SETTING_EXAMPLE_KEY_BINDING_SETTING_2", "LOC_MOD_SETTING_EXAMPLE_SETTING_TOOLTIP", 
+  {DisallowModifiers=true});
+
 
 -- Create enough category tabs that it will have to scroll.  And also add a bunch of settings to the example category.
 for i = 1, 20 do
-  ModSettings.Boolean:new(math.fmod(i, 2) == 0, tostring(i), 
+  ModSettings.Boolean:new(math.fmod(i, 2) == 0, "z" .. tostring(i), 
     "LOC_MOD_SETTING_EXAMPLE_BOOLEAN_SETTING", "LOC_MOD_SETTING_EXAMPLE_SETTING_TOOLTIP");
   local booleanSetting = ModSettings.Boolean:new(math.fmod(i, 2) == 0, "LOC_MOD_SETTING_EXAMPLE_CATEGORY", 
     tostring(i), "LOC_MOD_SETTING_EXAMPLE_SETTING_TOOLTIP");
+  local keybindSetting = ModSettings.KeyBinding:new(ModSettings.KeyBinding.MakeValue(Keys.K, {Alt=true, Ctrl=true}),
+    "z" .. tostring(i), "LOC_MOD_SETTING_EXAMPLE_KEY_BINDING_SETTING", "LOC_MOD_SETTING_EXAMPLE_SETTING_TOOLTIP");
 end
 
 local function PrintSettings()
@@ -53,9 +64,18 @@ rangeSetting:AddChangedHandler(PrintSettings);
 steppedRangeSetting:AddChangedHandler(PrintSettings);
 textSetting:AddChangedHandler(PrintSettings);
 selectSetting:AddChangedHandler(PrintSettings);
+actionSetting:AddChangedHandler(
+  function() 
+    print("Action!!!!!!");
+    booleanSetting:Change(not booleanSetting.Value);
+    booleanSetting2:Change(not booleanSetting2.Value);
+    rangeSetting:Change(math.max(rangeSetting.Value - 10, 0));
+    textSetting:Change(textSetting.Value .. '+');
+  end);
 
-ContextPtr:SetInputHandler(function(input) 
-    if keybindSetting:MatchesInput(input) then
+ContextPtr:SetInputHandler(
+  function(input) 
+    if KeyBindingHelper.InputMatches(keybindSetting.Value, input) then
       print("The bound key was pressed!");
       return true;
     end
